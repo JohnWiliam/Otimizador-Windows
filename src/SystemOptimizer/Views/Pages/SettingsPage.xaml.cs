2e89ec52ddd7af946aae85877aa2c31c93085576
John Wiliam <djhonathan.wilian@gmail.com>
2025-12-23 18:27:18 -0400

    Update SettingsPage.xaml.cs

--- src/SystemOptimizer/Views/Pages/SettingsPage.xaml.cs
+++ src/SystemOptimizer/Views/Pages/SettingsPage.xaml.cs
@@ -1,21 +1,20 @@
+using System.Windows.Controls; // Necessário para a classe Page padrão
 using Wpf.Ui.Controls;
 using SystemOptimizer.ViewModels;
 
 namespace SystemOptimizer.Views.Pages;
 
-public partial class SettingsPage : FluentPage
+public partial class SettingsPage : Page
 {
     public SettingsViewModel ViewModel { get; }
 
-    // O ViewModel será injetado automaticamente
     public SettingsPage(SettingsViewModel viewModel)
     {
         ViewModel = viewModel;
-        DataContext = this; // Permite ligar propriedades UI diretas se necessário
         
         InitializeComponent();
         
-        // Define o DataContext do XAML para o ViewModel
+        // Define o DataContext para o ViewModel injetado
         DataContext = ViewModel;
     }
 }
