ddc9bd94d3ae9d9328eeac7f02ff902f5aced4f1
John Wiliam <djhonathan.wilian@gmail.com>
2026-01-01 21:10:01 -0400

    Update SecurityPage.xaml.cs

--- src/SystemOptimizer/Views/Pages/SecurityPage.xaml.cs
+++ src/SystemOptimizer/Views/Pages/SecurityPage.xaml.cs
@@ -3,11 +3,11 @@ using Wpf.Ui.Abstractions.Controls;
 
 namespace SystemOptimizer.Views.Pages;
 
-public partial class SearchPage : INavigableView<MainViewModel>
+public partial class SecurityPage : INavigableView<MainViewModel>
 {
     public MainViewModel ViewModel { get; }
 
-    public SearchPage(MainViewModel viewModel)
+    public SecurityPage(MainViewModel viewModel)
     {
         ViewModel = viewModel;
         DataContext = this;
