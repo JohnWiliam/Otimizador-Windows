dde099c300189dac0952d43f2bada27d52ff99dc
John Wiliam <djhonathan.wilian@gmail.com>
2026-01-08 01:44:34 -0400

    Update SecurityPage.xaml.cs

--- src/SystemOptimizer/Views/Pages/SecurityPage.xaml.cs
+++ src/SystemOptimizer/Views/Pages/SecurityPage.xaml.cs
@@ -8,9 +8,6 @@ public partial class SecurityPage : Page
     public SecurityPage(MainViewModel viewModel)
     {
         InitializeComponent();
-
-        // Define o DataContext para o ViewModel injetado.
-        // Garante consistência com as outras páginas e funcionamento dos comandos.
         DataContext = viewModel;
     }
 }
