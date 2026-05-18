9ea9e7c3ce55d1d91edb462ff66e54ae3604fa12
John Wiliam <djhonathan.wilian@gmail.com>
2026-05-18 18:25:00 -0400

    Fix cleanup view model compile errors

--- src/SystemOptimizer/ViewModels/CleanupViewModel.cs
+++ src/SystemOptimizer/ViewModels/CleanupViewModel.cs
@@ -10,6 +10,7 @@ using System.Threading;
 using System.Threading.Tasks;
 using SystemOptimizer.Models;
 using SystemOptimizer.Properties;
+using SystemOptimizer.Services;
 
 namespace SystemOptimizer.ViewModels;
 
